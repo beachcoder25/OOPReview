ac5fa8524ead0537e1d6c0c71f5fcfdbdd9df687
beachcoder25 <cornish25@gmail.com>
2019-01-23 20:22:29 -0800

    Subclass

--- inheritance/cook.java
+++ inheritance/cook.java
@@ -12,7 +12,11 @@ public class cook extends employee{
 		
 		cook headCook = new cook();
 		
+		employee headCook2 = new cook();
+		
 		System.out.println("Salary: " + headCook.salary + "\nBonus: " + headCook.bonus);
+		System.out.println();
+		System.out.println("Salary: " + headCook2.salary + "\nBonus: N/A (invoked as Employee instance)");
 		
 	}
 
